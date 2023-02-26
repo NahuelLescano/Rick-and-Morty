6e9b9cadb7f90a33b58893803f1b980b1ab58465
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-02-26 16:21:59 -0300

    First steps for my proyect.

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card({ characters }) {
+   return (
+      <div>
+         <button onClick={}>X</button>
+         <h2></h2>
+         <h2></h2>
+         <h2></h2>
+         <img  src={} alt="" />
+      </div>
+   );
+}
---
+++ src/components/Cards.jsx
@@ -0,0 +1,5 @@
+import Card from './Card';
+
+export default function Cards({ characters }) {
+   return <div><Card characters={characters}></div>;
+}
