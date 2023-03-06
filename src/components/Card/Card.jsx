411de2fa08d05ced0099e9ae48c36dfd21e91be4
FacundoAylan <facundogsb@gmail.com>
2023-03-05 22:17:52 -0300

    Styling...

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,17 +3,19 @@ import Styles from './Card.module.css';
 export default function Card({ id, name, species,
                                genre, image, onClose }) {
    return (
-     <div className={Styles.container}>
       <div className={Styles.card}>
-         <button
-           className={Styles.btn}
-           onClick={onClose}
-         >X</button>
-        <img src={image} alt="name" className={Styles.img} />
+        <div className={Styles.containerImg}>
+          <button
+            className={Styles.btn}
+            onClick={onClose}
+          >X</button>
+          <img src={image} alt="name" className={Styles.img} />
+        </div>
+        <div className={Styles.containerInfo}>
          <h2>{name}</h2>
          <h2>{species}</h2>
          <h2>{genre}</h2>
+        </div>
       </div>
-     </div>
    );
 }
