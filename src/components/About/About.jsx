1f408a0369fb2527109152fc8105a744501d1762
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-03-12 21:12:54 -0300

    Putting an photo, links and styling about component.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,15 @@
 import Styles from './About.module.css';
+import image from '../../assets/profile-photo.jpeg';
 
 export default function About() {
   return (
     <div>
       <div className={Styles.container}>
         <h1>Welcome to my Rick and Morty web page!</h1>
+        <img
+        className={Styles.image}
+        src={image}
+        alt='profile' />
         <p>
           I'm full-stack student, and this is a proyect to practice what I learnt.
         </p>
@@ -12,6 +17,9 @@ export default function About() {
           I've also studied computer engineering.
         </p>
         <p>Created by: Nahuel</p>
+        <a href='https://www.linkedin.com/in/nahuel-lescano-906a2618b/'>Linkedin</a>
+        <a href='https://github.com/NahuelLescano'>My github</a>
+        <a href='https://github.com/NahuelLescano/Rick-and-Morty'>Repo from this project!</a>
       </div>
     </div>
   );
