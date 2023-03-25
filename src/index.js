b746b6e3cba1267b05757376af820b7ae81431ce
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-03-25 11:32:18 -0300

    Using createRoot instead of render only.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import App from './App';
 import store from './redux/store/store';
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+   <BrowserRouter>
+     <App />
+   </BrowserRouter>
+  </Provider>
 );
