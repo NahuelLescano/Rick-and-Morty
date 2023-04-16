07694ff1ab184c4da0f673cf8e4f819c6a2f6efb
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-04-16 19:48:24 -0300

    Fixing a bug...

--- Server/src/Controllers/getCharById.js
+++ Server/src/Controllers/getCharById.js
@@ -9,7 +9,7 @@ const getCharById = (res, id) => {
          const { id, name, status, species,
                  gender, origin, image } = response.data;
          res.writeHead(200, {"Content-type" : "Applicaton/json"})
-         res.end(JSON.stringify({ id, name, status, species, gender, oring, image }))
+         res.end(JSON.stringify({ id, name, status, species, gender, origin, image }))
        })
        .catch(reason => {
          res.writeHead(200, {"Content-type" : "Applicaton/json"})
