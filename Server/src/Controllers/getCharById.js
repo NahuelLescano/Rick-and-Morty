bfde1a11787b7ae89e9e568c9e91ca7afbb02125
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-04-17 20:27:14 -0300

    Refactoring to use express.

--- Server/src/Controllers/getCharById.js
+++ Server/src/Controllers/getCharById.js
@@ -1,20 +1,16 @@
 const axios = require('axios');
 
 const URL = 'https://rickandmortyapi.com/api/character/';
-const PORT = 3001;
 
-const getCharById = (res, id) => {
-  axios.get(`${URL}${id}`)
-       .then(response => {
-         const { id, name, status, species,
-                 gender, origin, image } = response.data;
-         res.writeHead(200, {"Content-type" : "Applicaton/json"})
-         res.end(JSON.stringify({ id, name, status, species, gender, origin, image }))
-       })
-       .catch(reason => {
-         res.writeHead(200, {"Content-type" : "Applicaton/json"})
-         res.end(JSON.stringify({ error: "Character not found" }));
-       });
+const async getCharById = (request, response) => {
+  const { id } = request.params;
+  try {
+    const response = await axios(`${URL}${id}`);
+    const { id, status, name, species, origin, image, gender } = response.data;
+    response.status(200).json({ id, status, name, species, origin, image, gender });
+  } catch(error) {
+    response.status(500).json({ message: error.message })
+  }
 }
 
 module.exports = getCharById;
