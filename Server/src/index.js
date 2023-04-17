5e5f421ca9a92bc4b0a1c1a84170608084897af1
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-04-17 20:27:31 -0300

    Using middlewares.

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,19 +1,25 @@
-const data = require('./Utils/Data');
-const http = require('http');
-const getCharById = require('./Controllers/getCharById');
+const express = require('express');
+const server = express();
 
-const PORT = 3001;
+const PORT = '3001';
 
-const server = http.createServer(({ url }, response) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    if (url.includes('/rickandmorty/character')) {
-      const id = parseInt(url.split('/').pop());
-      getCharById(response, id);
-    }
-  } catch(error) {
-    console.log(error);
-  }
-}).listen(PORT, 'localhost');
+server.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Credentials', 'true');
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  response.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
+  );
+
+  next();
+});
+server.use(express.json());
+server.use('/rickandmorty', router);
+
+server.listen(PORT, () => console.log(`Server raised in port: ${PORT}`));
 
 module.exports = server;
