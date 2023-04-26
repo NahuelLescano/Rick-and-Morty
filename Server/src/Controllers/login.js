1ebfb6db255e262fb1d2251a5cd5762fb7f66d00
nahuel <lescanoagustin10nahuel@gmail.com>
2023-04-26 00:01:24 -0300

    Changing message error.

--- Server/src/Controllers/login.js
+++ Server/src/Controllers/login.js
@@ -4,10 +4,10 @@ const login = (request, response) => {
   const { email, password } = request.query;
 
   try {
-    const found = users.find(user => user.email == email && user.password === password))
-    response.status(200).json({ access: true });
-  } catch(error) {
-    response.status(500).json({ access: false, message: error.message });
+    const found = users.find(user => user.email == email && user.password === password);
+    response.status(200).json({ access: found });
+  } catch({ message }) {
+    response.status(500).json({ message });
   }
 }
 
