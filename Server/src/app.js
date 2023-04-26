9c3ef8f2da3a9ba87588d9c51b3b4ab3d83c26fa
nahuel <lescanoagustin10nahuel@gmail.com>
2023-04-26 00:01:58 -0300

    Adding missing file.

---
+++ Server/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = require('./Routes/index');
+const server = express();
+
+server.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Credentials', 'true');
+    response.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    response.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, DELETE'
+    );
+  
+    next();
+  });
+
+  server.use(express.json());
+  server.use(morgan('dev'));
+  server.use('/rickandmorty', router);
+  
+  module.exports = server;
