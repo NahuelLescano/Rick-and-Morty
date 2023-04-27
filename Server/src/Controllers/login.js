7783cd8e7913d10365530f9252447f5ddb0a6cb1
nahuel <lescanoagustin10nahuel@gmail.com>
2023-04-27 07:56:31 -0300

    Minor changes.

--- Server/src/Controllers/login.js
+++ Server/src/Controllers/login.js
@@ -4,7 +4,7 @@ const login = (request, response) => {
   const { email, password } = request.query;
 
   try {
-    const found = users.find(user => user.email == email && user.password === password);
+    const found = users.find(user => user.email === email && user.password === password);
     response.status(200).json({ access: found });
   } catch({ message }) {
     response.status(500).json({ message });
