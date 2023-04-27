7b029a0de336b62c4bd4bd3d7df7a7f3e6f052d1
nahuel <lescanoagustin10nahuel@gmail.com>
2023-04-27 07:55:41 -0300

    Changing some things to fix a bug. Refactor this.

--- Server/src/Controllers/getCharById.js
+++ Server/src/Controllers/getCharById.js
@@ -2,15 +2,25 @@ const axios = require('axios');
 
 const URL = 'https://rickandmortyapi.com/api/character/';
 
-const getCharById = async (request, response) => {
-  const { id } = request.params;
+const getCharById = async (req, response) => {
+  const characterId = req.params.id;
   try {
-    const response = await axios(`${URL}${id}`);
-    const { id, status, name, species, origin, image, gender } = response.data;
+    const resp = await axios.get(`${URL}${characterId}`);
+    const { id, status, name, species, origin, image, gender } = resp.data;
     response.status(200).json({ id, status, name, species, origin, image, gender });
   } catch(error) {
-    response.status(500).json({ message: error.message })
+     response.status(500).send('error')
   }
 };
+// const getCharById = async (request, response) => {
+//   const { ID } = request.params;
+//   try {
+//     const res = await axios.get(`${URL}${ID}`);
+//     const { id, status, name, species, origin, image, gender } = res.data;
+//     response.status(200).json({ id, status, name, species, origin, image, gender });
+//   } catch(error) {
+//     response.status(500).json({ message: error.message })
+//   }
+// };
 
 module.exports = getCharById;
