5c323c366b13055ab30953d4296c0d58990b7eef
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-01 22:13:40 -0300

    Working with a real database.

--- Server/src/Controllers/login.js
+++ Server/src/Controllers/login.js
@@ -1,13 +1,32 @@
-const users = require('../Utils/users');
+// const users = require('../Utils/users');
+const { User } = require('../Model/User');
 
-const login = (request, response) => {
-  const { email, password } = request.query;
+const login = async (req, res) => {
+  const { email, password } = req.query;
+
+  if ((!email || email.trim() === '') &&
+     (!password || password.trim() === '')) {
+    return res.status(400).json({ message: 'Please provide email and password' });
+  }
+
+  const existingUser = await User.findOne({
+    where: { email },
+  });
+  if (!existingUser) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const existingUserEmail = await User.findOne({
+    where : { password },
+  });
+  if (!existingUserEmail) {
+    return res.status(404).json({ message: 'Incorrect password' });
+  }
 
   try {
-    const found = users.find(user => user.email === email && user.password === password);
-    response.status(200).json({ access: found });
+    res.status(200).json({ access: true });
   } catch({ message }) {
-    response.status(500).json({ message });
+    res.status(500).json({ message });
   }
 }
 
