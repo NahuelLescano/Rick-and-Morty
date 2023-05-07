ef19dbe988872f880d4cea4ff13f358a26e364fc
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-07 17:54:46 -0300

    Minor changes.

---
+++ Server/src/controllers/postUser.js
@@ -0,0 +1,20 @@
+const { User } = require('../db');
+
+const postUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || email.trim() === '' || !password || password.trim() === '') {
+    return res.status(400).json({ message: 'Missing or invalid data' });
+  }
+
+  try {
+    const [user] = await User.findOrCreate({
+      where: { email, password },
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { postUser };
