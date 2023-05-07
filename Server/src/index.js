40717c07b0afc9893594e0c33fe6ba6c5250cf81
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-07 18:02:01 -0300

    Minor changes.

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,9 @@
-const server = require('./app');
+const { server } = require('./app');
 const { conn } = require('./db');
 const { PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
-	server.listen(PORT, () => {
-		console.log(`Listening on port ${PORT}`);
-	});
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 });
