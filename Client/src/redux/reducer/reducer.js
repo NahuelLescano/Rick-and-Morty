a16b443059cdf4c89c8b6bc9282da1f412bd1e8a
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-07 17:58:50 -0300

    Updating reducer.

--- Client/src/redux/reducer/reducer.js
+++ Client/src/redux/reducer/reducer.js
@@ -1,16 +1,13 @@
-import {
-  ADD_FAV,
-  REMOVE_FAV,
-  FILTER,
-  ORDER } from '../actions/types';
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER, LOGIN } from '../actions/types';
 
 const initialState = {
+  idUser: 0,
   myFavorites: [],
   allCharacters: [],
 };
 
-const rootReducer = (state = initialState, { type, payload }) =>  {
-  switch(type) {
+const rootReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case ADD_FAV:
       return {
         ...state,
@@ -27,17 +24,29 @@ const rootReducer = (state = initialState, { type, payload }) =>  {
     case FILTER:
       return {
         ...state,
-        myFavorites: [...state.allCharacters.filter(character => character.gender === payload)]
+        myFavorites: [
+          ...state.allCharacters.filter(
+            (character) => character.gender === payload
+          ),
+        ],
       };
 
     case ORDER:
       return {
         ...state,
-        myFavorites: [...state.allCharacters.sort((a, b) => {
-          if (a.id > b.id) return payload === 'Ascending' ? 1 : -1;
-          if (a.id < b.id) return payload === 'Ascending' ? -1 : 1;
-          return 0;
-        })],
+        myFavorites: [
+          ...state.allCharacters.sort((a, b) => {
+            if (a.id > b.id) return payload === 'Ascending' ? 1 : -1;
+            if (a.id < b.id) return payload === 'Ascending' ? -1 : 1;
+            return 0;
+          }),
+        ],
+      };
+
+    case LOGIN:
+      return {
+        ...state,
+        idUser: payload,
       };
 
     default:
