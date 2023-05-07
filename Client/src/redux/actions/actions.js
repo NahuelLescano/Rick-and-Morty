7f6723cd87f5bce81e884a89fe0b5c6475531ca7
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-07 17:58:28 -0300

    Updating actions.

--- Client/src/redux/actions/actions.js
+++ Client/src/redux/actions/actions.js
@@ -1,34 +1,30 @@
 import axios from 'axios';
 
-import {
-  ADD_FAV,
-  REMOVE_FAV,
-  FILTER,
-  ORDER } from './types';
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER, LOGIN } from './types';
 
-export const addFav = character => {
+export const addFav = (character) => {
   const endpoint = 'http://localhost:3001/rickandmorty/fav';
-  return async dispatch => {
+  return async (dispatch) => {
     const { data } = await axios.post(endpoint, character);
     return dispatch({
       type: ADD_FAV,
       payload: data,
     });
-  }
+  };
 };
 
-export const removeFav = id => {
+export const removeFav = (id) => {
   const endpoint = `http://localhost:3001/rickandmorty/fav/${id}`;
-  return async dispatch => {
+  return async (dispatch) => {
     const { data } = await axios.delete(endpoint);
     return dispatch({
       type: REMOVE_FAV,
       payload: data,
     });
-  }
+  };
 };
 
-export const filterCards = gender => {
+export const filterCards = (gender) => {
   return {
     type: FILTER,
     payload: gender,
@@ -36,9 +32,22 @@ export const filterCards = gender => {
 };
 
 // A => ascending, D => descending
-export const orderCards = order => {
+export const orderCards = (order) => {
   return {
     type: ORDER,
     payload: order,
+  };
+};
+
+export const login = (email, password) => {
+  const endpoint = `http://localhost:3001/rickandmorty/login?email=${email}&password=${password}`;
+  return async (dispatch) => {
+    const { data } = await axios(endpoint);
+    if (data.success) {
+      dispatch({
+        type: LOGIN,
+        payload: data.id,
+      });
+    }
   };
 };
