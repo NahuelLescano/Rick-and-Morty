4290ed6ee483e3fa53a59cc04b641520e33dbab2
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-07 18:01:01 -0300

    Minor changes.

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
-const router = require('./routes/index');
+const { router } = require('./routes/index');
 const server = express();
 
-server.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Credentials', 'true');
-    response.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    response.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, DELETE'
-    );
-  
-    next();
-  });
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-reqed-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
-  server.use(express.json());
-  server.use(morgan('dev'));
-  server.use('/rickandmorty', router);
-  
-  module.exports = server;
+  next();
+});
+
+server.use(express.json());
+server.use(morgan('dev'));
+server.use('/rickandmorty', router);
+
+module.exports = { server };
