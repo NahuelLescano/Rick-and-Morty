eff36d2a3872604960660594d39e4514bc66fb40
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-07 18:03:17 -0300

    Updating routes.

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
-const getCharById = require('../Controllers/getCharById');
-const { postFav, deleteFav } = require('../Controllers/handleFavorites');
-const login = require('../Controllers/login');
+const { deleteFav } = require('../controllers/deleteFav');
+const { getCharById } = require('../controllers/getCharById');
+const { getFavs } = require('../controllers/getFavs');
+const { login } = require('../controllers/login');
+const { postFav } = require('../controllers/postFav');
+const { postUser } = require('../controllers/postUser');
 
 const router = Router();
 
 router.get('/character/:id', getCharById);
 router.get('/login', login);
+router.get('/fav', getFavs);
+
 router.post('/fav', postFav);
+router.post('/login', postUser);
+
 router.delete('/fav/:id', deleteFav);
 
-module.exports = router;
+module.exports = { router };
